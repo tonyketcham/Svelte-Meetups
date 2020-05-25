e07ee48fd1c76c16f2b98bf6354e2ee73c49a184
Tony Ketcham <ketcham@pnw.edu>
2020-05-25 06:32:20 -0500

    Added a text input component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
+  import TextInput from "./components/TextInput.svelte";
+
   import Header from "./components/Header.svelte";
   import MeetupGrid from "./meetup/Grid.svelte";
 
-  const meetups = [
+  let meetups = [
     {
       id: "1",
       title: "Creative Writing Workshop",
@@ -26,10 +28,73 @@
       contactEmail: "test@coding.com"
     }
   ];
+
+  let title = "";
+  let tagline = "";
+  let location = "";
+  let imageURL = "";
+  let contactEmail = "";
+  let description = "";
+
+  const addMeetup = () => {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      tagline: tagline,
+      location: location,
+      imageURL: imageURL,
+      contactEmail: contactEmail,
+      description: description
+    };
+
+    meetups = [newMeetup, ...meetups];
+  };
 </script>
 
 <Header />
 <body>
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      type="text"
+      value={title}
+      on:input={event => (title = event.target.value)} />
+    <TextInput
+      id="tagline"
+      label="Tagline"
+      type="text"
+      value={tagline}
+      on:input={event => (tagline = event.target.value)} />
+    <TextInput
+      id="location"
+      label="Location"
+      type="text"
+      value={location}
+      on:input={event => (location = event.target.value)} />
+    <TextInput
+      id="imageURL"
+      label="Image URL"
+      type="text"
+      value={imageURL}
+      on:input={event => (imageURL = event.target.value)} />
+    <TextInput
+      id="contactEmail"
+      label="Contact Email"
+      type="email"
+      value={contactEmail}
+      on:input={event => (contactEmail = event.target.value)} />
+    <TextInput
+      id="description"
+      label="Description"
+      controlType="textarea"
+      rows="3"
+      value={description}
+      on:input={event => (description = event.target.value)} />
+
+    <button type="submit">Publish</button>
+
+  </form>
   <MeetupGrid {meetups} />
 </body>
 
---
+++ src/components/TextInput.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let label;
+  export let id;
+  export let value;
+  export let controlType;
+  export let type;
+  export let rows;
+</script>
+
+<div class="form-control">
+  <label for={id}>{label}</label>
+  {#if controlType === 'textarea'}
+    <textarea {rows} {id} {value} on:input />
+  {:else}
+    <input {type} {id} {value} on:input />
+  {/if}
+</div>
+
+<style>
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: none;
+    border-bottom: 2px solid #ccc;
+    border-radius: 3px 3px 0 0;
+    color: white;
+    background: #2e273e;
+    padding: 0.15rem 0.25rem;
+    transition: border-color 0.1s ease-out;
+  }
+
+  input:focus,
+  textarea:focus {
+    border-color: #e40763;
+    outline: none;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+    width: 100%;
+    color: white;
+  }
+
+  .form-control {
+    padding: 0.5rem 0;
+    width: 100%;
+    margin: 0.25rem 0;
+  }
+</style>
