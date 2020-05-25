8181d091c1dee263cddeb2d923ed982ae35b2fb5
tonyketcham <ketcham@pnw.edu>
2020-05-25 15:41:37 -0500

    A clickable icon

---
+++ src/components/ButtonIcon.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let type;
+  export let href;
+  export let style;
+</script>
+
+{#if href}
+  <a {href} class={style}>
+    <slot style="width: 100%;">
+      <p>Default Button Icon</p>
+    </slot>
+  </a>
+{:else}
+  <button {type} class={style}>
+    <slot style="width: 100%;">
+      <p>Default Button Icon</p>
+    </slot>
+  </button>
+{/if}
+
+<style>
+  button {
+    width: 36pt;
+    background: transparent;
+    border: transparent;
+  }
+  .icon {
+    width: 45px;
+  }
+
+  .pink-outline {
+    fill: #d75b61;
+  }
+</style>
