9fa054ba1e472e90742fc4c75c4f3a142ce1fd3c
Tony Ketcham <ketcham@pnw.edu>
2020-06-04 04:04:37 -0500

    Removed default bias in styling

--- src/meetup/Form.svelte
+++ src/meetup/Form.svelte
@@ -1,6 +1,7 @@
 <script>
   import TextInput from "../components/TextInput.svelte";
   import Button from "../components/Button.svelte";
+  import Modal from "../components/Modal.svelte";
   import { createEventDispatcher } from "svelte";
 
   const dispatch = createEventDispatcher();
@@ -22,59 +23,53 @@
   };
 </script>
 
-<form on:submit|preventDefault={() => dispatch('newMeetup', meetup)}>
-  <TextInput
-    id="title"
-    label="Title"
-    type="text"
-    value={title}
-    on:input={event => (meetup.title = event.target.value)}
-    required />
-  <TextInput
-    id="tagline"
-    label="Tagline"
-    type="text"
-    value={tagline}
-    on:input={event => (meetup.tagline = event.target.value)}
-    required />
-  <TextInput
-    id="location"
-    label="Location"
-    type="text"
-    value={location}
-    on:input={event => (meetup.location = event.target.value)}
-    required />
-  <TextInput
-    id="imageURL"
-    label="Image URL"
-    type="text"
-    value={imageURL}
-    on:input={event => (meetup.imageURL = event.target.value)}
-    required />
-  <TextInput
-    id="contactEmail"
-    label="Contact Email"
-    type="email"
-    value={contactEmail}
-    on:input={event => (meetup.contactEmail = event.target.value)}
-    required />
-  <TextInput
-    id="description"
-    label="Description"
-    controlType="textarea"
-    rows="3"
-    value={description}
-    on:input={event => (meetup.description = event.target.value)}
-    required />
+<Modal title="Publish a Meetup">
+  <form on:submit|preventDefault={() => dispatch('newMeetup', meetup)}>
+    <TextInput
+      id="title"
+      label="Title"
+      type="text"
+      value={title}
+      on:input={event => (meetup.title = event.target.value)}
+      required />
+    <TextInput
+      id="tagline"
+      label="Tagline"
+      type="text"
+      value={tagline}
+      on:input={event => (meetup.tagline = event.target.value)}
+      required />
+    <TextInput
+      id="location"
+      label="Location"
+      type="text"
+      value={location}
+      on:input={event => (meetup.location = event.target.value)}
+      required />
+    <TextInput
+      id="imageURL"
+      label="Image URL"
+      type="text"
+      value={imageURL}
+      on:input={event => (meetup.imageURL = event.target.value)}
+      required />
+    <TextInput
+      id="contactEmail"
+      label="Contact Email"
+      type="email"
+      value={contactEmail}
+      on:input={event => (meetup.contactEmail = event.target.value)}
+      required />
+    <TextInput
+      id="description"
+      label="Description"
+      controlType="textarea"
+      rows="3"
+      value={description}
+      on:input={event => (meetup.description = event.target.value)}
+      required />
 
-  <Button type="submit">Publish</Button>
-  <Button on:click={() => dispatch('cancel', true)}>Cancel</Button>
-</form>
-
-<style>
-  form {
-    width: 33%;
-    margin: auto;
-    margin-bottom: 2rem;
-  }
-</style>
+    <Button type="submit">Publish</Button>
+    <Button on:click={() => dispatch('cancel', true)}>Cancel</Button>
+  </form>
+</Modal>
